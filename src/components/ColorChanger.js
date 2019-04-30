8be3124e01fa15778aabeb7879f6e84c2cd1748f
Ian Kaliszewski <ikalisz@yahoo.com>
2019-04-30 09:18:52 -0600

    disable edit does not work

--- src/components/ColorChanger.js
+++ src/components/ColorChanger.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function ColorChanger(props) {
     return (
-      <select  onChange={(e) => props.update(e.target.value)} className="dropDownContainer" disabled={props.allowEdit === "false"}>
+      <select   className="dropDownContainer" onChange={(e) => props.update(e.target.value)} disabled={props.allowEdit === "false"}>
         <option value="black"> Black </option>
         <option value="blue"> Blue </option>
         <option value="green"> Green </option>
